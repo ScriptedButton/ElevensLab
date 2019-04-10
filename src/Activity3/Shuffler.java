8a18b36dad57373d495b42d78ff9f23517375459
coleb <coleb@DESKTOP-LDSFCLM>
2019-04-09 21:24:47 -0400

    Complete Shuffler

--- src/Activity3/Shuffler.java
+++ src/Activity3/Shuffler.java
@@ -3,6 +3,8 @@
  */
 package Activity3;
 
+import java.util.Random;
+
 public class Shuffler {
 
 	/**
@@ -62,7 +64,21 @@ public class Shuffler {
 	 * @param values is an array of integers simulating cards to be shuffled.
 	 */
 	public static void perfectShuffle(int[] values) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 3 *** */
+		int[] shuffled = new int[52];
+
+		int k = 0;
+
+		for (int j = 0; j < (values.length + 1) / 2; j++){
+			values[j] = shuffled[k];
+			k+=2;
+		}
+
+		k = 1;
+
+		for (int j = (values.length + 1) / 2; j < values.length; j++){
+			values[j] = shuffled[k];
+			k+=2;
+		}
 	}
 
 	/**
@@ -77,6 +93,9 @@ public class Shuffler {
 	 * @param values is an array of integers simulating cards to be shuffled.
 	 */
 	public static void selectionShuffle(int[] values) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 3 *** */
+		for (int k = values.length-1; k > 1; k--){
+			int r = (int)(Math.random() * k+1);
+			values[k] = values[r];
+		}
 	}
 }
